aec6ee95e58acbf961e769f4ba6ddbcb9f3dcf68
shiggs22 <shaguay@fullsail.edu>
2014-06-08 19:36:04 -0400

    added player one and player two info
    added var for player and a 2 that contains the name, damage per player
    and also, the health the players start off with

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -7,7 +7,19 @@ Assignment: Goal1: Assignment: Duel1
 // self executing function
 (function(){
 
-    console.log("FIGHT!!!")
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoDamage = 100;
 
 })();
 
