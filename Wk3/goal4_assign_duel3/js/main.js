668a841e7c230365993d433c7a50d017bf77092f
shiggs22 <shaguay@fullsail.edu>
2014-06-20 20:16:08 -0400

    created function
    created fight function

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -12,31 +12,47 @@
 function () {
     console.log("FIGHT!");
 
-    var fighter1_txt = document.querySelector("#ww").querySelector("p");
+    var fighter1_txt = document.querySelector("#kabal").querySelector("p");
     var fighter2_txt = document.querySelector("#bw").querySelector("p");
     var round_txt = document.querySelector("h4");
     var button = document.getElementById("fight_btn");
+
+
+    button.addEventListener("click", fight, false);
+
+
+    var fighters = [
+
+        {
+            name: "Kabal",
+            damage: 20,
+            health: 100
+
+        },
+        {
+            name: "Kratos",
+            damage: 20,
+            health: 100
+
+        }
+    ];
+    var round = 1;
+
+    round_txt.innerHTML = "Click FIGHT to start!";
+    fighter1_txt.innerText = fighters[0].name + ":" + fighters[0].health;
+    fighter2_txt.innerText = fighters[1].name + ":" + fighters[1].health;
 }
 
-button.addEventListener("click", fight, false);
+function fight(){
 
+    fighter1_txt.innerHTML = fighters[0].name + ":" + fighters[0].health;
+    fighter2_txt.innerHTML = fighters[1].name + ":" + fighters[1].health;
 
-var fighters = [
+        var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage * .5);
+        var f2 = Math.floor(Math.random() * fighters[1].damage + fighters[1].damage * .5);
 
-    {
-        name:"Kabal",
-        damage:20,
-        health:100
+        fighters[0].health -=f1;
+        fighters[1].health -=f2;
 
-    },
-    {
-        name:"Kratos",
-        damage:20,
-        health:100
-
-    }];
-var round = 1;
-
-round_txt.innerHTML = "Click FIGHT to start!";
-fighter1_txt.innerText = fighters[0].name + ":" + fighters[0].health;
-fighter2_txt.innerText = fighters[1].name + ":" + fighters[1].health;
+        console.log(fighters[0].health, fighters[1].health);
+}
