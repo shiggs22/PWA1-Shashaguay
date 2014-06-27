ea34b9b9fd8c21328ed087f1cef9f38a3b8d10d6
shiggs22 <shaguay@fullsail.edu>
2014-06-27 18:15:56 -0400

    created function
    created the main function to include the jobs, actions, and the global
    object for the site

--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -3,3 +3,22 @@
  * 06/27/2014
  * Goal: Goal7
  */
+
+
+(function(){
+    // global window object for the main.js file
+    window.Persons=Person;
+    // description of the people's jobs and the actions they take during the time frame
+    Person.jobs = ["fighter","trainer","student","military"];
+    Person.actions = ["sleeping","training","working"];
+
+
+
+
+
+
+
+
+
+
+}
