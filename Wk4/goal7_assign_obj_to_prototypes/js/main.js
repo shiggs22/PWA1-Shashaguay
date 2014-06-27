c93a9a2a71730d6d29f78dc3f4df0caa552cf34c
shiggs22 <shaguay@fullsail.edu>
2014-06-27 19:10:32 -0400

    created main function in main.js
    created main function in main.js which includes var setup for
    numOfPeople, people, names and interval

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -4,3 +4,56 @@
  * Goal: Goal7
  */
 
+
+(Function(){
+
+    var numOfPeople = 3;
+    var people = [];
+    var names = ["Nicholas","Sean","Shane","Paul","Andrea"],
+    var interval;
+
+
+    for (var i = 0; i < numOfPeople ; i++) {
+
+        var personIndex = Math.floor(Math.random()*names.length);
+
+        var person = new Person(names[personIndex], i+1);
+
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+
+        people.push(person);
+        names.splice(personIndex,1);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -5,50 +5,51 @@
  */
 
 
-(function(){
+(function() {
 
     // global window object for the main.js file
-    window.Persons=Person;
+    window.Person=Person;
 
 
     // description of the people's jobs and the actions they take during the time frame
-    Person.jobs = ["fighter","trainer","student","military"];
-    Person.actions = ["sleeping","training","working"];
+    Person.jobs = ["fighter", "trainer", "student", "military"];
+    Person.actions = ["sleeping", "training", "working"];
 
 
-   function Person(name,row){
-       console.log("Person Created: ", name);
+    function Person(name, row) {
+        console.log("Person Created: ", name);
 
-       this.name = name;
+        this.name = name;
 
-       // set actions for each person randomly
-       this.action = Person.actions[Math.floor(Math.random()*Persons.actions.length)];
+        // set actions for each person randomly
+        this.action = Person.actions[Math.floor(Math.random() * Person.actions.length)];
 
-       // set jobs for each person randomly
-       this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+        // set jobs for each person randomly
+        this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];
 
-       // sets the rows for JS in HTML to update the text
-       this.row = row;
+        // sets the rows for JS in HTML to update the text
+        this.row = row;
 
-       // displays action
-       var id = document.getElementById("r"+this.row+"c3");
-       id.innerHTML=this.action;
+        // displays action
+        var id = document.getElementById("r"+this.row+"c3");
+        id.innerHTML = this.action;
 
-   }
+    }
 
     // prototype created to function 30 times a second thus randomly determines the action change for each person
-    Person.prototype.update = function(){
-        if (Math.floor(Math.random() <.01)){
-            var i = Math.floor(Math.random()*Person.actions.length);
+    Person.prototype.update = function () {
+        if (Math.floor(Math.random() < .01)) {
+            var i = Math.floor(Math.random() * Person.actions.length);
             this.action = Person.actions[i];
             var id = document.getElementById("r"+this.row+"c3");
-            id.innerHTML=this.actions;
+            id.innerHTML = this.actions;
         }
 
     }
 
 
+})();
 
 
 
-}
+
